38f6642eb510f822dea63d1e74409d2667d55dd7
NotBotProductions <56902425+NotBotProductions@users.noreply.github.com>
2020-02-16 00:03:28 -0500

    src-video11-initial-course-project-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { AddPetComponent } from './add-pet/add-pet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PetListComponent,
+    AddPetComponent
   ],
   imports: [
     BrowserModule,
-		FormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
